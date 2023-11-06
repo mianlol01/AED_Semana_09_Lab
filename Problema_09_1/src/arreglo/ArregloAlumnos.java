52e7de3a06efca408f971483a2cf4d347f06f434
mianlol01 <snake.1503@outlook.com>
2023-11-05 21:50:43 -0500

    dasfs

--- Problema_09_1/src/arreglo/ArregloAlumnos.java
+++ Problema_09_1/src/arreglo/ArregloAlumnos.java
@@ -1,37 +1,92 @@
 package arreglo;
 
+import clase.Alumno;
 import java.util.ArrayList;
 
-import clase.Alumno;
-
 public class ArregloAlumnos {
-	//  Atributo privado
-	private ArrayList <Alumno> alu;
-	//  Constructor
-    public ArregloAlumnos() {
-        alu = new ArrayList <Alumno> ();
-    	adicionar(new Alumno(123, "Ana", 19, 15));
-    	adicionar(new Alumno(456, "Juan", 15, 14));
-    	adicionar(new Alumno(789, "Pedro", 17, 19));
-    	adicionar(new Alumno(302, "David", 13, 18));
-    	adicionar(new Alumno(417, "Carlos", 20, 19));
-    	adicionar(new Alumno(641, "Jorge", 12, 13));
-    	adicionar(new Alumno(208, "María", 15, 17));
-    	adicionar(new Alumno(820, "José", 11, 10));
-    }
-	//  Operaciones públicas básicas
+	private ArrayList<Alumno> alu;
+
+	public ArregloAlumnos() {
+		alu = new ArrayList<Alumno>();
+		adicionar(new Alumno(123, "Ana", 19, 15));
+		adicionar(new Alumno(456, "Juan", 15, 14));
+		adicionar(new Alumno(789, "Pedro", 17, 19));
+		adicionar(new Alumno(302, "David", 13, 18));
+		adicionar(new Alumno(417, "Carlos", 20, 19));
+		adicionar(new Alumno(641, "Jorge", 12, 13));
+		adicionar(new Alumno(208, "MarÃ­a", 15, 17));
+		adicionar(new Alumno(820, "JosÃ©", 11, 10));
+	}
+
 	public void adicionar(Alumno x) {
 		alu.add(x);
 	}
+
 	public int tamanio() {
 		return alu.size();
 	}
+
 	public Alumno obtener(int i) {
 		return alu.get(i);
 	}
-	//  Operaciones públicas complementarias
-	public int cantAprobados() {
-    	return 0;
-    }
-}
 
+	public int cantAprobados() {
+		int count = 0;
+		for (Alumno alumno : alu) {
+			if (alumno.promedio() >= 13) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public int cantDesaprobados() {
+		int count = 0;
+		for (Alumno alumno : alu) {
+			if (alumno.promedio() < 13) {
+				count++;
+			}
+		}
+		return count;
+	}
+
+	public String nombrePrimerDesaprobado() {
+		for (Alumno alumno : alu) {
+			if (alumno.promedio() < 13) {
+				return alumno.getNombre();
+			}
+		}
+		return null;
+	}
+
+	public String nombreUltimoAprobado() {
+		for (int i = alu.size() - 1; i >= 0; i--) {
+			if (alu.get(i).promedio() >= 13) {
+				return alu.get(i).getNombre();
+			}
+		}
+		return null;
+	}
+
+	public String nombreSegundoDesaprobado() {
+		int desaprobadosCount = 0;
+		for (Alumno alumno : alu) {
+			if (alumno.promedio() < 13) {
+				desaprobadosCount++;
+				if (desaprobadosCount == 2) {
+					return alumno.getNombre();
+				}
+			}
+		}
+		return null;
+	}
+
+	public String nombrePenultimoAprobado() {
+		for (int i = alu.size() - 2; i >= 0; i--) {
+			if (alu.get(i).promedio() >= 13) {
+				return alu.get(i).getNombre();
+			}
+		}
+		return null;
+	}
+}
--- Problema_09_1/src/gui/Problema_09_1.java
+++ Problema_09_1/src/gui/Problema_09_1.java
@@ -19,11 +19,12 @@ import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
+import javax.swing.UIManager;
 
 public class Problema_09_1 extends JFrame implements ActionListener {
-	
+
 	private static final long serialVersionUID = 1L;
-	
+
 	private JPanel contentPane;
 	private JLabel lblCodigo;
 	private JLabel lblNombre;
@@ -31,16 +32,16 @@ public class Problema_09_1 extends JFrame implements ActionListener {
 	private JLabel lblNota2;
 	private JTextField txtCodigo;
 	private JTextField txtNombre;
-	private JTextField txtNota1;	
+	private JTextField txtNota1;
 	private JTextField txtNota2;
-	private JButton btnAdicionar;	
+	private JButton btnAdicionar;
 	private JButton btnReportar;
 	private JScrollPane scrollPaneA;
 	private JScrollPane scrollPaneB;
 	private JTextArea txtS;
 	private JTable tblTabla;
 	private DefaultTableModel modelo;
-	
+
 	/**
 	 * Launch the application.
 	 */
@@ -48,10 +49,10 @@ public class Problema_09_1 extends JFrame implements ActionListener {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+					UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
 					Problema_09_1 frame = new Problema_09_1();
 					frame.setVisible(true);
-				}
-				catch (Exception e) {
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
@@ -69,33 +70,33 @@ public class Problema_09_1 extends JFrame implements ActionListener {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-	
+
 		lblCodigo = new JLabel("C\u00F3digo");
 		lblCodigo.setBounds(10, 11, 40, 28);
 		contentPane.add(lblCodigo);
-		
+
 		lblNombre = new JLabel("Nombre");
 		lblNombre.setBounds(120, 11, 50, 28);
-		contentPane.add(lblNombre);	
-		
+		contentPane.add(lblNombre);
+
 		lblNota1 = new JLabel("Nota 1");
 		lblNota1.setBounds(255, 11, 40, 28);
-		contentPane.add(lblNota1);	
-		
+		contentPane.add(lblNota1);
+
 		lblNota2 = new JLabel("Nota 2");
 		lblNota2.setBounds(358, 11, 40, 28);
-		contentPane.add(lblNota2);	
+		contentPane.add(lblNota2);
 
 		txtCodigo = new JTextField();
 		txtCodigo.setBounds(54, 11, 40, 28);
 		contentPane.add(txtCodigo);
 		txtCodigo.setColumns(10);
-	
+
 		txtNombre = new JTextField();
 		txtNombre.setBounds(170, 11, 60, 28);
 		contentPane.add(txtNombre);
 		txtNombre.setColumns(10);
-		
+
 		txtNota1 = new JTextField();
 		txtNota1.setBounds(295, 11, 40, 28);
 		contentPane.add(txtNota1);
@@ -110,22 +111,22 @@ public class Problema_09_1 extends JFrame implements ActionListener {
 		btnAdicionar.addActionListener(this);
 		btnAdicionar.setBounds(450, 50, 155, 23);
 		contentPane.add(btnAdicionar);
-		
+
 		btnReportar = new JButton("Reportar");
 		btnReportar.setBounds(450, 75, 155, 23);
 		btnReportar.addActionListener(this);
 		contentPane.add(btnReportar);
-		
+
 		scrollPaneA = new JScrollPane();
 		scrollPaneA.setBounds(10, 50, 432, 200);
 		contentPane.add(scrollPaneA);
-		
+
 		tblTabla = new JTable();
 		tblTabla.setFillsViewportHeight(true);
 		scrollPaneA.setViewportView(tblTabla);
 
 		modelo = new DefaultTableModel();
-		modelo.addColumn("código");
+		modelo.addColumn("cï¿½digo");
 		modelo.addColumn("nombre");
 		modelo.addColumn("nota 1");
 		modelo.addColumn("nota 2");
@@ -135,17 +136,17 @@ public class Problema_09_1 extends JFrame implements ActionListener {
 		scrollPaneB = new JScrollPane();
 		scrollPaneB.setBounds(10, 250, 432, 100);
 		contentPane.add(scrollPaneB);
-		
+
 		txtS = new JTextArea();
 		txtS.setFont(new Font("Monospaced", Font.PLAIN, 13));
 		scrollPaneB.setViewportView(txtS);
-		
-		listar();		
+
+		listar();
 	}
-	
-	//  Declaración global
+
+	// Declaraciï¿½n global
 	ArregloAlumnos aa = new ArregloAlumnos();
-	
+
 	public void actionPerformed(ActionEvent arg0) {
 		if (arg0.getSource() == btnReportar) {
 			actionPerformedBtnReportar(arg0);
@@ -154,19 +155,28 @@ public class Problema_09_1 extends JFrame implements ActionListener {
 			actionPerformedBtnAdicionar(arg0);
 		}
 	}
+
 	protected void actionPerformedBtnAdicionar(ActionEvent arg0) {
 		/**
 		 * Adiciona un nuevo alumno
-		 */	
-		int codigo = leerCodigo();
-		String nombre = leerNombre();
-		int nota1 = leerNota1();
-		int nota2 = leerNota2();
-		Alumno nuevo = new Alumno(codigo, nombre, nota1, nota2);
-		aa.adicionar(nuevo);
-		listar();
-		limpieza();
+		 */
+		try {
+			
+			int codigo = leerCodigo();
+			String nombre = leerNombre();
+			int nota1 = leerNota1();
+			int nota2 = leerNota2();
+			if(nota1 >= 0 && nota1 <=20 && nota2 >= 0 && nota2 <=20) {
+			Alumno nuevo = new Alumno(codigo, nombre, nota1, nota2);
+			aa.adicionar(nuevo);
+			listar();
+			limpieza();}
+			else {mensaje("error de ingreso");}
+		} catch (Exception e) {
+			mensaje("error de ingreso");
+		}
 	}
+
 	protected void actionPerformedBtnReportar(ActionEvent arg0) {
 		/**
 		 * Visualiza un reporte
@@ -174,11 +184,13 @@ public class Problema_09_1 extends JFrame implements ActionListener {
 		txtS.setText("");
 		imprimir("cantidad de alumnos :  " + aa.tamanio());
 		imprimir("cantidad de aprobados :  " + aa.cantAprobados());
-  	}
-	//  Métodos tipo void (sin parámetros)
+	}
+
+	// Mï¿½todos tipo void (sin parï¿½metros)
 	void imprimir() {
 		imprimir("");
 	}
+
 	void limpieza() {
 		txtCodigo.setText("");
 		txtNombre.setText("");
@@ -187,36 +199,40 @@ public class Problema_09_1 extends JFrame implements ActionListener {
 		txtS.setText("");
 		txtCodigo.requestFocus();
 	}
-   	void listar() {
+
+	void listar() {
 		modelo.setRowCount(0);
-		for (int i=0; i<aa.tamanio(); i++) {
-			Object[] fila = { aa.obtener(i).getCodigo(),
-					          aa.obtener(i).getNombre(),
-					          aa.obtener(i).getNota1(),
-					          aa.obtener(i).getNota2(),
-					          aa.obtener(i).promedio() };
+		for (int i = 0; i < aa.tamanio(); i++) {
+			Object[] fila = { aa.obtener(i).getCodigo(), aa.obtener(i).getNombre(), aa.obtener(i).getNota1(),
+					aa.obtener(i).getNota2(), aa.obtener(i).promedio() };
 			modelo.addRow(fila);
 		}
 	}
-	//  Métodos tipo void (con parámetros)
+
+	// Mï¿½todos tipo void (con parï¿½metros)
 	void imprimir(String s) {
 		txtS.append(s + "\n");
 	}
+
 	void mensaje(String s) {
 		JOptionPane.showMessageDialog(this, s);
-	}		
-	//  Métodos que retornan valor (sin parámetros)
+	}
+
+	// Mï¿½todos que retornan valor (sin parï¿½metros)
 	int leerCodigo() {
 		return Integer.parseInt(txtCodigo.getText().trim());
 	}
+
 	String leerNombre() {
 		return txtNombre.getText().trim();
 	}
+
 	int leerNota1() {
 		return Integer.parseInt(txtNota1.getText().trim());
 	}
+
 	int leerNota2() {
 		return Integer.parseInt(txtNota2.getText().trim());
 	}
-	
+
 }
